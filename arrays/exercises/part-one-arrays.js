7239c61ad709d7aef93ecc21bcad0653f0ffca2a
Derrick Milliot <merlot5390@gmail.com>
2023-09-17 20:13:12 -0500

    array exercises completed

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,16 @@
 //Create an array that can hold 4 items name practiceFile.
+let practiceFile = ['yellow', 'apple', 'gorge', 'purple'];
 
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+let new1 = 42;
+let new2 = 'hello';
+
+practiceFile[4] = new1;
+console.log(practiceFile);
+practiceFile[5] = new2;
+console.log(practiceFile);
 
 //Use a SetValue to add the items "false", and "-4.6" to the array.  Print the array to confirm the changes.
+
+practiceFile.push(false, -4.6, "87");
+console.log(practiceFile);
