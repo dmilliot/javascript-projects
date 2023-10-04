0fdf58e78731ab27e424c73852b08c42e75218d2
Derrick Milliot <merlot5390@gmail.com>
2023-10-04 17:19:56 -0500

    module exercise complete

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,11 @@
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+  let randomPicked = Math.floor(Math.random()*arr.length);
+  let output = arr[randomPicked];
+  return output;
 }
 
 //TODO: Export the randomFromArray function.
+module.exports = {
+  randomFromArray: randomFromArray
+}
